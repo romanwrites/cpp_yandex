2f69ad38c859dc076117f5feea81bb2685390224
kukinpower <thekukinpower@gmail.com>
2021-01-18 02:15:08 +0300

    fix: BadArgumentException

--- 05 Final task/FinalTask.cpp
+++ 05 Final task/FinalTask.cpp
@@ -5,31 +5,31 @@
 #include <sstream>
 using namespace std;
 
+class BadArgumentException : exception {
+public:
+	explicit BadArgumentException(string const &msg) {message = msg;}
+	BadArgumentException() {message = "";}
+
+	virtual const char* what() const throw() {
+		return message.c_str();
+	}
+
+private:
+	string message;
+};
+
 class Date {
 public:
 	Date(int year, int month, int day) {
 		if (month < 1 || month > 12) {
-			throw Date::BadArgumentException("Month value is invalid: " + to_string(month));
+			throw BadArgumentException("Month value is invalid: " + to_string(month));
+		} else if (day < 1 || day > 31) {
+			throw BadArgumentException("Day value is invalid: " + to_string(day));
 		}
-
 		this->year = year;
 		this->month = month;
 		this->day = day;
 	}
-
-	class BadArgumentException : exception {
-	public:
-		BadArgumentException(string msg) {message = msg;}
-		BadArgumentException() {message = "";}
-
-		virtual const char* what() const throw()
-		{
-			return message.c_str();
-		}
-
-	private:
-		string message;
-	};
 
 	int GetYear() const {return year;}
 	int GetMonth() const {return month;}
@@ -47,51 +47,48 @@ private:
 	}
 };
 
-//bool operator<(const Date& lhs, const Date& rhs);
-//
-//class Database {
-//public:
-//	void AddEvent(const Date& date, const string& event);
-//	bool DeleteEvent(const Date& date, const string& event);
-//	int  DeleteDate(const Date& date);
-//
-//	/* ??? */ Find(const Date& date) const;
-//
-//	void Print() const;
-//
-//private:
-//	map<Date, string> m;
-//
-//};
+bool operator<(const Date& lhs, const Date& rhs);
+
+class Database {
+public:
+	void AddEvent(const Date& date, const string& event);
+	bool DeleteEvent(const Date& date, const string& event);
+	int  DeleteDate(const Date& date);
+
+	/* ??? */ Find(const Date& date) const;
+
+	void Print() const;
+
+private:
+	map<Date, string> m;
+
+};
 
 int main() {
-	try {
-		Date d = {21, 0, 3};
-	} catch (exception &ex) {
-		cout << ex.what() <<endl;
+  Database db;
+
+  string command;
+  while (getline(cin, command)) {
+  	try {
+		istringstream input(command);
+		if (input) {
+			string cmd;
+			input >> cmd;
+			if (cmd == "Add") {
+				try {
+					string date, event;
+					input >> date >> event;
+				} catch (exception &ex) {
+					cout << ex.what() << endl;
+				}
+			} else {
+				throw BadArgumentException("Unknown command: " + cmd);
+			}
+		}
+  	} catch (BadArgumentException &ex) {
+		cout << ex.what() << endl;
 	}
+  }
 
+  return 0;
 }
-
-//int main() {
-//  Database db;
-//
-//  string command;
-//  while (getline(cin, command)) {
-//	istringstream input(command);
-//	if (input) {
-//		string cmd;
-//		input >> cmd;
-//		if (cmd == "Add") {
-//			try {
-//				string date, event;
-//				input >> date >> event;
-//			} catch (exception &ex) {
-//				cout << ex.what() << endl;
-//			}
-//		}
-//	}
-//  }
-//
-//  return 0;
-//}
