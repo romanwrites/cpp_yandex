eb4960baa5bab6bf733a7d4d59c78e536c0d4602
kukinpower <thekukinpower@gmail.com>
2021-02-16 02:20:59 +0300

    feat: solve functions definition task

---
+++ 02_Yellow_belt/01 Integers, tuples, pairs, templates/13_Sum_reverse_sort.cpp
@@ -0,0 +1,31 @@
+#include <string>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+//#include "sum_reverse_sort.h"
+
+using namespace std;
+
+int Sum(int x, int y) {
+    return x + y;
+}
+
+string Reverse(string s) {
+    return string(s.rbegin(), s.rend());
+}
+
+void Sort(vector<int>& nums) {
+    sort(nums.begin(), nums.end());
+}
+
+int main() {
+    cout << Sum(10, 1) << endl;
+    cout << Reverse("Zoom-zoom") << endl;
+    vector<int> v = {5, 4, 3, 2, 1};
+    Sort(v);
+    for (auto const &i : v) {
+        cout << i << " ";
+    }
+    cout << endl;
+    return 0;
+}
