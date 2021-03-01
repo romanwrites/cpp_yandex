a4b7098222b802a415eadd527e38f15f4528d6c5
kukinpower <thekukinpower@gmail.com>
2021-03-01 04:08:50 +0300

    refactor: uncomment PrintStats code

--- 02_Yellow_belt/04 Iterators, queue, deque/22_PrintStats.cpp
+++ 02_Yellow_belt/04 Iterators, queue, deque/22_PrintStats.cpp
@@ -1,34 +1,34 @@
-//#include <iostream>
-//#include <vector>
-//using namespace std;
-//
-//enum class Gender {
-//  FEMALE,
-//  MALE
-//};
-//
-//struct Person {
-//  int age;  // возраст
-//  Gender gender;  // пол
-//  bool is_employed;  // имеет ли работу
-//};
+#include <iostream>
+#include <vector>
+using namespace std;
+
+enum class Gender {
+  FEMALE,
+  MALE
+};
+
+struct Person {
+  int age;  // возраст
+  Gender gender;  // пол
+  bool is_employed;  // имеет ли работу
+};
 
 // Это пример функции, его не нужно отправлять вместе с функцией PrintStats
-//template <typename InputIt>
-//int ComputeMedianAge(InputIt range_begin, InputIt range_end) {
-//  if (range_begin == range_end) {
-//    return 0;
-//  }
-//  vector<typename InputIt::value_type> range_copy(range_begin, range_end);
-//  auto middle = begin(range_copy) + range_copy.size() / 2;
-//  nth_element(
-//      begin(range_copy), middle, end(range_copy),
-//      [](const Person& lhs, const Person& rhs) {
-//        return lhs.age < rhs.age;
-//      }
-//  );
-//  return middle->age;
-//}
+template <typename InputIt>
+int ComputeMedianAge(InputIt range_begin, InputIt range_end) {
+  if (range_begin == range_end) {
+    return 0;
+  }
+  vector<typename InputIt::value_type> range_copy(range_begin, range_end);
+  auto middle = begin(range_copy) + range_copy.size() / 2;
+  nth_element(
+      begin(range_copy), middle, end(range_copy),
+      [](const Person& lhs, const Person& rhs) {
+        return lhs.age < rhs.age;
+      }
+  );
+  return middle->age;
+}
 
 void PrintStats(vector<Person> persons) {
 
@@ -64,20 +64,20 @@ void PrintStats(vector<Person> persons) {
   cout << "Median age for unemployed males = "
        << ComputeMedianAge(employed_males_end, end(persons))      << endl;
 }
-//
-//void PrintStats(vector<Person> persons);
-//
-//int main() {
-//  vector<Person> persons = {
-//      {31, Gender::MALE, false},
-//      {40, Gender::FEMALE, true},
-//      {24, Gender::MALE, true},
-//      {20, Gender::FEMALE, true},
-//      {80, Gender::FEMALE, false},
-//      {78, Gender::MALE, false},
-//      {10, Gender::FEMALE, false},
-//      {55, Gender::MALE, true},
-//  };
-//  PrintStats(persons);
-//  return 0;
-//}
+
+void PrintStats(vector<Person> persons);
+
+int main() {
+  vector<Person> persons = {
+      {31, Gender::MALE, false},
+      {40, Gender::FEMALE, true},
+      {24, Gender::MALE, true},
+      {20, Gender::FEMALE, true},
+      {80, Gender::FEMALE, false},
+      {78, Gender::MALE, false},
+      {10, Gender::FEMALE, false},
+      {55, Gender::MALE, true},
+  };
+  PrintStats(persons);
+  return 0;
+}
