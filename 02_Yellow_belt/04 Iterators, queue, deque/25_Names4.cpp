9508986ca3cd4b3f7d67a17c3f9f5f22aee20185
kukinpower <thekukinpower@gmail.com>
2021-03-02 03:47:08 +0300

    feat: add Names4

---
+++ 02_Yellow_belt/04 Iterators, queue, deque/25_Names4.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <iterator>
+#include <map>
+using namespace std;
+
+class Person {
+ public:
+  void ChangeFirstName(int year, const string& first_name) {
+    first_names[year] = first_name;
+  }
+  void ChangeLastName(int year, const string& last_name) {
+    last_names[year] = last_name;
+  }
+  string GetFullName(int year) {
+    string const first_name = findName(first_names, year);
+    string const last_name = findName(last_names, year);
+
+    if (first_name.empty() && last_name.empty()) {
+      return "Incognito";
+    } else if (first_name.empty()) {
+      return last_name + " with unknown first name";
+    } else if (last_name.empty()) {
+      return first_name + " with unknown last name";
+    } else {
+      return first_name + " " + last_name;
+    }
+  }
+ private:
+  map<int, string> first_names;
+  map<int, string> last_names;
+
+  string findName(map<int, string> const &m, int year) {
+    auto it =  m.upper_bound(year);
+    if (it != m.begin()) {
+      return prev(it)->second;
+    }
+    return "";
+  }
+};
+
+int main() {
+  Person person;
+
+  person.ChangeFirstName(1965, "Polina");
+  person.ChangeLastName(1967, "Sergeeva");
+  for (int year : {1900, 1965, 1990}) {
+    cout << person.GetFullName(year) << endl;
+  }
+
+  person.ChangeFirstName(1970, "Appolinaria");
+  for (int year : {1969, 1970}) {
+    cout << person.GetFullName(year) << endl;
+  }
+
+  person.ChangeLastName(1968, "Volkova");
+  for (int year : {1969, 1970}) {
+    cout << person.GetFullName(year) << endl;
+  }
+
+  return 0;
+}
